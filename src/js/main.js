ea0ce1a0d97f6b39cdd5f74327cfc7e1b9e21adf
yuriipereverziev <y.pereverzyev@everad.com>
2022-01-04 19:04:14 +0200

    add mobile blocks header & promo

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,27 @@
 import './helpers/postDate';
 import scrollSmooth from './helpers/smooth-scroll.js';
 import burger from './modules/burger-menu';
-import reviews from './modules/reviews';
-import footer from './modules/footer';
-import banner from './modules/banner';
+// import reviews from './modules/reviews';
+// import footer from './modules/footer';
+// import banner from './modules/banner';
 
 
 function main() {
     scrollSmooth();
     burger();
-    reviews()
-    footer();
-    banner();
+    // reviews()
+    // footer();
+    // banner();
 }
 
-if (document.documentElement.clientWidth < 480) {
-    window.addEventListener('scroll',
-        function () {
-            return setTimeout(main, 1000);
-        }, {
-            once: true
-        });
-} else {
+// if (document.documentElement.clientWidth < 480) {
+//     window.addEventListener('scroll',
+//         function () {
+//             return setTimeout(main, 1000);
+//         }, {
+//             once: true
+//         });
+// } else {
+
     main();
-}
+// }
---
+++ src/js/modules/burger-menu.js
@@ -0,0 +1,18 @@
+export default function () {
+
+    let ham = document.querySelector('.burger');
+    let nav = document.querySelector('.header__nav');
+
+    ham.addEventListener('click', function () {
+        nav.classList.toggle('active');
+        activateBurger()
+    })
+
+    function activateBurger() {
+        document.querySelector('.burger-top').classList.toggle('burger-top-click');
+        document.querySelector('.burger-middle').classList.toggle('burger-middle-click');
+        document.querySelector('.burger-bottom').classList.toggle('burger-bottom-click');
+    }
+};
+
+
